d04e272eed26dd776308685568cfd6689f3c722f
Cleyton-sousa <cleyton.santos@b2bti.com>
2024-07-04 01:16:44 -0300

    add docs on path .../doc

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Api docs')
+    .setDescription('Api de tarefas com auth de usuarios')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+  await app.listen(3000);
+}
+bootstrap();
